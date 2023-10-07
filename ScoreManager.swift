97c193ac337d3c7f28432f85db31159c677b3bd4
Krishi <krishi3dalal@gmail.com>
2023-10-06 20:22:43 -0400

    commit

--- ScoreManager.swift
+++ ScoreManager.swift
@@ -8,9 +8,11 @@
 import Foundation
 class ScoreManager {
     private let scoreKey = "com.SustainableSteppers.score"
+    //function to save score gained from take action now page
     func saveScore(_ score: Int) {
         UserDefaults.standard.set(score, forKey: scoreKey)
     }
+    //function to display score gained from take action now page
     func loadScore() -> Int {
         return UserDefaults.standard.integer(forKey: scoreKey)
     }
